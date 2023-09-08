f4cbfdea9ded616c09e5a8d1bb1fd0d36dc673d7
sunny kumar <sunnykethavath@gmail.com>
2023-09-08 15:06:44 +0530

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,39 @@
+package org.example;
+
+import org.json.JSONObject;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class Main {
+    public static void main(String[] args) {
+        String myUrl = "https://api.chucknorris.io/jokes/random";
+        try {
+            URL url = new URL(myUrl);
+            HttpURLConnection connect = (HttpURLConnection) url.openConnection();
+            int responseCode = connect.getResponseCode();
+
+            if(responseCode == 200){
+                BufferedReader in = new BufferedReader(new InputStreamReader(connect.getInputStream()));
+                StringBuilder apiData = new StringBuilder();
+                String line;
+
+                while((line = in.readLine()) != null){
+                    apiData.append(line);
+                }
+                in.close();
+
+                JSONObject jsonObj = new JSONObject(apiData.toString());
+                String joke = jsonObj.getString("value");
+                System.out.println(joke);
+            }else{
+                System.out.println("Failed to fetch data. HTTP Response Code: " + responseCode);
+            }
+            connect.disconnect();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
